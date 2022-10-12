4ee0942aeeefd974480298044937954b23a1cf49
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-10-12 23:47:44 +0600

    Some style added (inline too)

--- mini-blog/src/App.js
+++ mini-blog/src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+
 import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  
+
   return (
     <div className="App">
       <Navbar/>
--- mini-blog/src/Navbar.js
+++ mini-blog/src/Navbar.js
@@ -1,10 +1,15 @@
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>Rafee's Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <a href="/create" style = {{
+                    color: "white",
+                    backgroundColor: '#f13f6d',
+                    borderRadius: '8px'
+                }}>New Blog</a>
             </div>
         </nav>
      );
