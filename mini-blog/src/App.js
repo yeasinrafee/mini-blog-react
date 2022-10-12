87d7353226084ffeafa9adec19c70e8f7cd46be2
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-10-12 23:38:03 +0600

    Working with multiple components

--- mini-blog/src/App.js
+++ mini-blog/src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = "Hey, welcome to my new Blog";
-  const subscribers = 90;
-  const link = "https://www.google.com";
-
-
+  
   return (
     <div className="App">
+      <Navbar/>
      <div className="content">
-      <h1> {title} </h1>
-      <p>There are {subscribers} subscribers </p>
-      <a href={link}> Google </a>
+      <Home/>
       </div> 
     </div>
   );
---
+++ mini-blog/src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return ( 
+        <div className="home">
+            <h1>HomePage</h1>
+        </div>
+     );
+}
+ 
+export default Home;
