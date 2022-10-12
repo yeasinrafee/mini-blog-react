a657eb7cddb5778d857f70204d85214b593a10d3
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-10-13 01:16:42 +0600

    Button-handle

--- mini-blog/src/Home.js
+++ mini-blog/src/Home.js
@@ -1,7 +1,19 @@
+
+
 const Home = () => {
+    const handleClick = ()=>{
+        console.log("What is up dude!");
+    }
+
+    const handleClickAgain = (name)=>{
+        console.log(`hey, ${name}`);
+    }
+
     return ( 
         <div className="home">
             <h1>HomePage</h1>
+            <button className="btn-click" onClick = {handleClick}>Click Here</button>
+            <button className="btn-click" onClick = {() => handleClickAgain('Rafee')}>Click Here</button>
         </div>
      );
 }
