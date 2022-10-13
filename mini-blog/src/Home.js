89edc83f36fbdeb4c3f9c0f919969abbdf68b399
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-10-13 22:49:44 +0600

    Function as props

--- mini-blog/src/Home.js
+++ mini-blog/src/Home.js
@@ -4,13 +4,18 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState([
         {title: "Hola Hola Hola", body: "lorem imsum....", author: "Rafee", id: 1}, 
-        {title: "This is Pred", body: "lorem imsum....", author: "Yeasin", id: 1},
-        {title: "Who Wants to be a king?", body: "lorem imsum....", author: "Ragner", id: 1},
+        {title: "This is Pred", body: "lorem imsum....", author: "Yeasin", id: 2},
+        {title: "Who Wants to be a king?", body: "lorem imsum....", author: "Ragner", id: 3},
     ])
+
+    const handleDelete = (id) =>{
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
 
     return ( 
         <div className="home">           
-            <BlogList blogs= {blogs}/>
+            <BlogList blogs= {blogs} title = "All Blogs: " handleDelete={handleDelete}/>
         </div>
      );
 }
