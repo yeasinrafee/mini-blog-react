027ed50aa929dd27cdeef77b65700492deb15583
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-10-13 11:34:59 +0600

    working with props

--- mini-blog/src/Home.js
+++ mini-blog/src/Home.js
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -8,14 +9,8 @@ const Home = () => {
     ])
 
     return ( 
-        <div className="home">
-            {blogs.map((blog) =>(
-                <div className="blog-preview" key = {blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by: {blog.author}</p>
-                </div>
-            ))}
-            
+        <div className="home">           
+            <BlogList blogs= {blogs}/>
         </div>
      );
 }
