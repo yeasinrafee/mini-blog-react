ac086bb633a47f7c9968b1bc72b921be37815d4e
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-10-13 23:05:07 +0600

    useEffect hook

--- mini-blog/src/Home.js
+++ mini-blog/src/Home.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -13,9 +13,18 @@ const Home = () => {
         setBlogs(newBlogs);
     }
 
+    const [name, setName] = useState("Rafee");
+
+    useEffect(() =>{
+        console.log("useEffect ran");
+        console.log(name);
+    }, [name])
+
     return ( 
         <div className="home">           
             <BlogList blogs= {blogs} title = "All Blogs: " handleDelete={handleDelete}/>
+            <button onClick={()=> setName("Yeasin")}>Change</button>
+            <p>{name}</p>
         </div>
      );
 }
