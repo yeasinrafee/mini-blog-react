be7d356ae8c118c4b59b565510552eda9b805169
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-10-15 01:47:37 +0600

    Router Link added

--- mini-blog/src/Navbar.js
+++ mini-blog/src/Navbar.js
@@ -1,15 +1,16 @@
+import {Link} from 'react-router-dom';
 
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>Rafee's Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" style = {{
+                <Link to="/">Home</Link>
+                <Link to="/create" style = {{
                     color: "white",
                     backgroundColor: '#f13f6d',
                     borderRadius: '8px'
-                }}>New Blog</a>
+                }}>New Blog</Link>
             </div>
         </nav>
      );
