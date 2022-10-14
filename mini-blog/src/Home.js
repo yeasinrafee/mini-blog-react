9b6a953903fbefc66b706c9362f34f809586f3a4
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-10-15 01:01:29 +0600

    Handling the errors

--- mini-blog/src/Home.js
+++ mini-blog/src/Home.js
@@ -1,22 +1,39 @@
-import {useState, useEffect} from 'react';
-import BlogList from './BlogList';
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);   
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() =>{
-        fetch('http://localhost:8000/blogs')
-        .then((res)=> res.json())
-        .then((data)=>{
-            setBlogs(data);
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Could not fetch the data...");
+          }
+          return res.json();
         })
-    }, [])
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+        });
+    }, 1000);
+  }, []);
 
-    return ( 
-        <div className="home">           
-            {blogs && <BlogList blogs= {blogs} title = "All Blogs: "/>}
-        </div>
-     );
-}
- 
+  return (
+    <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs: " />}
+    </div>
+  );
+};
+
 export default Home;
