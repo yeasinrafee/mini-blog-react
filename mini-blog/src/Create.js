23b24baa37ac6763627b7739c615ca01aa28438d
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-10-15 13:45:54 +0600

    Making POST request

--- mini-blog/src/Create.js
+++ mini-blog/src/Create.js
@@ -1,13 +1,27 @@
 import { useState } from "react";
+import {useHistory} from 'react-router-dom';
 
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
 
     const handleSubmit = (e) =>{
         e.preventDefault();
         const blog = {title, body, author};
+
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs/', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(()=>{
+            setIsPending(false);
+            history.push('/');
+        })
     }
 
     return ( 
@@ -35,7 +49,8 @@ const Create = () => {
                     <option value ="mario">mario</option>
                     <option value ="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding Blog...</button>}
             </form>
         </div>
      );
