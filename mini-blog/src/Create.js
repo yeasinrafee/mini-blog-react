72970912da4b4c8ed5ac1f4f90a480a58f12866e
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-10-15 13:08:15 +0600

    new blog form updated

--- mini-blog/src/Create.js
+++ mini-blog/src/Create.js
@@ -2,7 +2,24 @@
 const Create = () => {
     return ( 
         <div className="create">
-            <h2>Create a blog</h2>
+            <h2>Add a New Blog</h2>
+            <form>
+                <lebel>Blog title: </lebel>
+                <input
+                    type='text'
+                    required
+                />
+                <lebel>Blog body: </lebel>
+                <textarea 
+                    required
+                ></textarea>
+                <lebel>Blog author: </lebel>
+                <select>
+                    <option value ="mario">mario</option>
+                    <option value ="yoshi">yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
      );
 }
